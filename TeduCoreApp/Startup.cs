cf86a761c8d6732322e6d0653c908e50e4167e6f
Toanbn <ngoctoan89@gmail.com>
2017-11-02 21:57:15 +0700

    Create admin area

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -101,6 +101,9 @@ namespace TeduCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
           
         }
