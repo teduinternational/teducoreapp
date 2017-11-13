a3fcf04948faf8b00a591d3e2a3044b1bbebf733
Toanbn <ngoctoan89@gmail.com>
2017-11-13 07:47:42 +0700

    integrate CK Editor

--- TeduCoreApp/wwwroot/app/controllers/product/index.js
+++ TeduCoreApp/wwwroot/app/controllers/product/index.js
@@ -3,6 +3,7 @@
         loadCategories();
         loadData();
         registerEvents();
+        registerControls();
     }
 
     function registerEvents() {
@@ -64,7 +65,7 @@
                     $('#txtOriginalPriceM').val(data.OriginalPrice);
                     $('#txtPromotionPriceM').val(data.PromotionPrice);
 
-                   // $('#txtImageM').val(data.ThumbnailImage);
+                    // $('#txtImageM').val(data.ThumbnailImage);
 
                     $('#txtTagM').val(data.Tags);
                     $('#txtMetakeywordM').val(data.SeoKeywords);
@@ -72,7 +73,7 @@
                     $('#txtSeoPageTitleM').val(data.SeoPageTitle);
                     $('#txtSeoAliasM').val(data.SeoAlias);
 
-                    //CKEDITOR.instances.txtContentM.setData(data.Content);
+                    CKEDITOR.instances.txtContent.setData(data.Content);
                     $('#ckStatusM').prop('checked', data.Status == 1);
                     $('#ckHotM').prop('checked', data.HotFlag);
                     $('#ckShowHomeM').prop('checked', data.HomeFlag);
@@ -134,7 +135,7 @@
                 var seoPageTitle = $('#txtSeoPageTitleM').val();
                 var seoAlias = $('#txtSeoAliasM').val();
 
-                //var content = CKEDITOR.instances.txtContentM.getData();
+                var content = CKEDITOR.instances.txtContent.getData();
                 var status = $('#ckStatusM').prop('checked') == true ? 1 : 0;
                 var hot = $('#ckHotM').prop('checked');
                 var showHome = $('#ckShowHomeM').prop('checked');
@@ -151,7 +152,7 @@
                         OriginalPrice: originalPrice,
                         PromotionPrice: promotionPrice,
                         Description: description,
-                        Content: '',
+                        Content: content,
                         HomeFlag: showHome,
                         HotFlag: hot,
                         Tags: tags,
@@ -183,6 +184,27 @@
             }
 
         });
+
+    }
+
+    function registerControls() {
+        CKEDITOR.replace('txtContent', {});
+
+        //Fix: cannot click on element ck in modal
+        $.fn.modal.Constructor.prototype.enforceFocus = function () {
+            $(document)
+                .off('focusin.bs.modal') // guard against infinite focus loop
+                .on('focusin.bs.modal', $.proxy(function (e) {
+                    if (
+                        this.$element[0] !== e.target && !this.$element.has(e.target).length
+                        // CKEditor compatibility fix start.
+                        && !$(e.target).closest('.cke_dialog, .cke').length
+                        // CKEditor compatibility fix end.
+                    ) {
+                        this.$element.trigger('focus');
+                    }
+                }, this));
+        };
 
     }
     function initTreeDropDownCategory(selectedId) {
