801cc6d1eca737c7f71b461c3f2bfc99fe0a1de2
Toanbn <ngoctoan89@gmail.com>
2017-12-12 22:17:38 +0700

    add some common components

---
+++ TeduCoreApp/Controllers/Components/MainMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeduCoreApp.Controllers.Components
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
