ea06e301d78c490f6f82dfde231cdbd3b7a00bd0
Toanbn <ngoctoan89@gmail.com>
2017-12-19 08:03:22 +0700

    Load product catalog

--- TeduCoreApp/Controllers/ProductController.cs
+++ TeduCoreApp/Controllers/ProductController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TeduCoreApp.Models.ProductViewModels;
+using TeduCoreApp.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace TeduCoreApp.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        IConfiguration _configuration;
+        public ProductController(IProductService productService,IConfiguration configuration,
+            IProductCategoryService productCategoryService)
+        {
+            _productService = productService;
+            _productCategoryService = productCategoryService;
+            _configuration = configuration;
+        }
         [Route("products.html")]
         public IActionResult Index()
         {
@@ -15,7 +28,23 @@ namespace TeduCoreApp.Controllers
         }
 
         [Route("{alias}-c.{id}.html")]
-        public IActionResult Catalog(int id, string keyword, int? pageSize, string sortBy, int page = 1)
+        public IActionResult Catalog(int id, int? pageSize, string sortBy, int page = 1)
+        {
+            var catalog = new CatalogViewModel();
+            ViewData["BodyClass"] = "shop_grid_full_width_page";
+            if (pageSize == null)
+                pageSize = _configuration.GetValue<int>("PageSize");
+
+            catalog.PageSize = pageSize;
+            catalog.SortType = sortBy;
+            catalog.Data = _productService.GetAllPaging(id, string.Empty, page, pageSize.Value);
+            catalog.Category = _productCategoryService.GetById(id);
+
+            return View(catalog);
+        }
+
+        [Route("{alias}-p.{id}.html", Name = "ProductDetail")]
+        public IActionResult Details(int id)
         {
             return View();
         }
