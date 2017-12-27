d5bd4a74f217bacde2c294b0fd8ce18f900e8e96
Toanbn <ngoctoan89@gmail.com>
2017-12-27 07:50:25 +0700

    Searching product

--- TeduCoreApp/Controllers/ProductController.cs
+++ TeduCoreApp/Controllers/ProductController.cs
@@ -43,6 +43,23 @@ namespace TeduCoreApp.Controllers
             return View(catalog);
         }
 
+
+        [Route("search.html")]
+        public IActionResult Search(string keyword, int? pageSize, string sortBy, int page = 1)
+        {
+            var catalog = new SearchResultViewModel();
+            ViewData["BodyClass"] = "shop_grid_full_width_page";
+            if (pageSize == null)
+                pageSize = _configuration.GetValue<int>("PageSize");
+
+            catalog.PageSize = pageSize;
+            catalog.SortType = sortBy;
+            catalog.Data = _productService.GetAllPaging(null, keyword, page, pageSize.Value);
+            catalog.Keyword = keyword;
+
+            return View(catalog);
+        }
+
         [Route("{alias}-p.{id}.html", Name = "ProductDetail")]
         public IActionResult Details(int id)
         {
